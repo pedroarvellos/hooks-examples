dc3319958f759dd2b3331169f73ec6f17a03dfa6
Pedro Arvellos <pedro.goncalves@watchguard.com>
2019-10-31 20:58:17 -0300

    Add Toolbar

--- react-hooks-demos/src/App.js
+++ react-hooks-demos/src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 import TodoApp from './TodoApp'
+import Paper from '@material-ui/core/Paper'
 import "./App.css";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const app = props => {
-  return <TodoApp/>
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa"
+      }}
+      elevation={0}
+    >
+      <AppBar color='primary' position='static' style={{height: "64px"}}>
+        <Toolbar>
+          <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+    </Paper>
+  )
 }
 
 export default app;
