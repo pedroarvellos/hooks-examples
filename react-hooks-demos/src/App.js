1c9ae9efac0c5951e53cd73bcae482717e5d7b78
Pedro Arvellos <pedro.goncalves@watchguard.com>
2019-10-31 21:05:24 -0300

    Define useState hook

--- react-hooks-demos/src/App.js
+++ react-hooks-demos/src/App.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
 import TodoApp from './TodoApp'
 import Paper from '@material-ui/core/Paper'
-import "./App.css";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import './App.css';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 const app = props => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      task: 'Clean Fishtank',
+      completed: false
+    },
+    {
+      id: 2,
+      task: 'Wash the car',
+      completed: true
+    },
+    {
+      id: 3,
+      task: 'Grow Beard',
+      completed: 'false'
+    }
+  ])
+
   return (
     <Paper
       style={{
         padding: 0,
         margin: 0,
-        height: "100vh",
-        backgroundColor: "#fafafa"
+        height: '100vh',
+        backgroundColor: '#fafafa'
       }}
       elevation={0}
     >
-      <AppBar color='primary' position='static' style={{height: "64px"}}>
+      <AppBar color='primary' position='static' style={{ height: '64px' }}>
         <Toolbar>
           <Typography color='inherit'>TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
+      {/* <TodoList todos={todos}>
+
+      </TodoList> */}
     </Paper>
   )
 }
