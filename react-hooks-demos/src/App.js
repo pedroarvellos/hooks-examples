51006474c28d60c3fc367508a8744f9abbc05c86
Pedro Arvellos <pedro.goncalves@watchguard.com>
2019-11-01 12:08:21 -0300

    Add new methods

--- react-hooks-demos/src/App.js
+++ react-hooks-demos/src/App.js
@@ -3,6 +3,7 @@ import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 import './App.css';
 import { AppBar, Toolbar, Typography, Paper, Grid } from '@material-ui/core';
+import uuid from 'uuid/v4'
 
 const app = props => {
   const [todos, setTodos] = useState([
@@ -24,7 +25,29 @@ const app = props => {
   ])
 
   const addTodo = newTodoText => {
-    setTodos([...todos, { id: 4, task: newTodoText, completed: false }])
+    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }])
+  }
+
+  const removeTodo = todoId => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId)
+
+    setTodos(updatedTodos)
+  }
+
+  const toggleTodo = todoId => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    )
+
+    setTodos(updatedTodos)
+  }
+
+  const editTodo = (todoId, newTask) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === todoId ? { ...todo, task: newTask } : todo
+    )
+
+    setTodos(updatedTodos)
   }
 
   return (
@@ -42,10 +65,15 @@ const app = props => {
           <Typography color='inherit'>TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
-      <Grid container justify='center' style={{marginTop: '1rem'}}>
+      <Grid container justify='center' style={{ marginTop: '1rem' }}>
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo}/>
-          <TodoList todos={todos} />
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+          />
         </Grid>
       </Grid>
     </Paper>
