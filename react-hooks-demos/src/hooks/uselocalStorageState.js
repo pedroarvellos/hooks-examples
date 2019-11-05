70fc289cb19e71509b6ab33a05d8df514d20d1d0
Pedro Arvellos <pedro.goncalves@watchguard.com>
2019-11-05 20:02:14 -0300

    Create useState implementation

--- react-hooks-demos/src/hooks/uselocalStorageState.js
+++ react-hooks-demos/src/hooks/uselocalStorageState.js
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react'
 
 function UseLocalStorageState(key, defaultValue) {
-    
+    const [state, setState] = useState(() => {
+        let val
+        try{
+            val = JSON.parse(window.localStorage.getItem(key) || String(defaultValue))
+        } catch(e) {
+            val = defaultValue
+        }
+        return val
+    })
+
+
 }
 
 export default UseLocalStorageState
